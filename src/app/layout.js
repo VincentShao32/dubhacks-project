39ecf3c0445fe56537464a2c52524c55cdacaa67
VincentShao32 <yunbo2016@gmail.com>
2024-10-12 16:46:15 -0700

    navbar styling

--- src/app/layout.js
+++ src/app/layout.js
@@ -6,11 +6,29 @@ import { UserProvider } from "@auth0/nextjs-auth0/client";
 
 import Navbar from "./components/Navbar";
 
+const geistSans = localFont({
+  src: "./fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+  weight: "100 900",
+});
+const geistMono = localFont({
+  src: "./fonts/GeistMonoVF.woff",
+  variable: "--font-geist-mono",
+  weight: "100 900",
+});
+
+const satoshiVariable = localFont({
+  src: "./fonts/Satoshi-Variable.woff",
+  variable: "--font-satoshi-variable",
+});
+
 export default function RootLayout({ children }) {
   return (
     <UserProvider>
       <html lang="en">
-        <body>
+        <body
+          className={`${geistSans.variable} ${geistMono.variable} ${satoshiVariable.variable} antialiased`}
+        >
           <Navbar></Navbar>
           <header>
             {/* <SignedOut>
