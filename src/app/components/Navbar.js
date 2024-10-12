e25bfb92ff8c84d3ad5af212ec4c08485604eed2
VincentShao32 <yunbo2016@gmail.com>
2024-10-12 13:44:20 -0700

    auth0 setup complete

---
+++ src/app/components/Navbar.js
@@ -0,0 +1,7 @@
+const Navbar = () => {
+  return (
+    <div className="fixed w-full flex bg-blue-500 p-4 text-white">navbar</div>
+  );
+};
+
+export default Navbar;
--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,20 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <div className="">
-      <p>hi</p>
-    </div>
+    <ul>
+      <li>
+        <Link href="/">Home</Link>
+      </li>
+      <li>
+        <Link href="/about">About Us</Link>
+      </li>
+      <li>
+        <Link href="/api/auth/login">Login</Link>
+      </li>
+      <li>
+        <a href="/api/auth/logout">Logout</a>
+      </li>
+    </ul>
   );
 }
