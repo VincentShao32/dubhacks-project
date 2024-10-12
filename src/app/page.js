78b3c37aa525709a75973017862e13dd1312de25
Yanda Bao <yandabaodav@gmail.com>
2024-10-12 13:46:52 -0700

    group order schema and functions

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,13 @@
 export default function Home() {
+  let a = 5;
+  const handleOpenGroupWindowCreate = () => {
+    
+  }
+
   return (
     <div className="">
-      <p>hi</p>
+      <p>hi there, please wait for Google Maps integration</p>
+      <button onClick={handleOpenGroupWindowCreate}></button>
     </div>
   );
 }
