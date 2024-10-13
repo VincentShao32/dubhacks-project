9b4d27089c2621a7a6667f7cf3dfa4ea162279a0
VincentShao32 <yunbo2016@gmail.com>
2024-10-12 21:08:24 -0700

    home page tweaks

--- src/app/page.js
+++ src/app/page.js
@@ -13,7 +13,7 @@ export default function Home() {
           <h1 className="font-[family-name:var(--font-satoshi-variable)] text-[10rem] text-red leading-none ">
             Datch
           </h1>
-          <h1 className=" text-4xl text-red">
+          <h1 className=" text-4xl text-red pb-4">
             The more you share, the more you save
           </h1>
           <a
@@ -27,8 +27,8 @@ export default function Home() {
       </div>
       <div className="w-full bg-red">
         <div className="flex justify-between pl-8 py-8">
-          <Image src={image2} />
-          <div className="w-[1200px] bg-white">
+          <Image src={image2} className="object-cover w-[33%]" />
+          <div className="w-[65%] bg-white flex items-center pl-8">
             <div className="flex flex-col p-8 gap-8">
               <h1 className="font-[family-name:var(--font-satoshi-variable)] text-6xl text-red">
                 Expensive delivery?
@@ -44,7 +44,7 @@ export default function Home() {
           </div>
         </div>
         <div className="flex justify-between pr-8 pb-8">
-          <div className="w-[1100px] bg-white flex items-center pl-8">
+          <div className="w-[65%] bg-white flex items-center pl-8">
             <div className="flex flex-col p-8 gap-8">
               <h1 className="font-[family-name:var(--font-satoshi-variable)] text-6xl text-red">
                 Connect over your favorite foods.
@@ -58,7 +58,7 @@ export default function Home() {
               </p>
             </div>
           </div>
-          <Image src={image3} className="object-contain h-[380px]" />
+          <Image src={image3} className="object-contain w-[33%]" />
         </div>
       </div>
     </div>
